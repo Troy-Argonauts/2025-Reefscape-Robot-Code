a31aecda0fa539a832c71e5b8014f304914ea7f7
Evan13019 <137565148+Evan13019@users.noreply.github.com>
2025-02-08 17:29:13 -0800

    update to periodic, also commented out until further notice

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,28 +7,24 @@ public class Climber extends SubsystemBase{
     private TalonFX armMotor1, armMotor2, alignMotor, tongueMotor;
     private DigitalInput armLimit, tongueLimit, alignLimit;
 
-    private DoubleLogEntry armMotor1SupplyCurrentLog;
-    private DoubleLogEntry armMotor2SupplyCurrentLog;
-    private DoubleLogEntry alignMotorSupplyCurrentLog;
-    private DoubleLogEntry tongueMotorSupplyCurrentLog;
-
     public Climber() {}
 
     @Override
     public void periodic() {
         //needs motors instantiated and methods written
-        armMotor1SupplyCurrentLog.append(armMotor1.getSupplyCurrent().getValueAsDouble());
-        armMotor2SupplyCurrentLog.append(armMotor2.getSupplyCurrent().getValueAsDouble());
-        alignMotorSupplyCurrentLog.append(alignMotor.getSupplyCurrent().getValueAsDouble());
-        tongueMotorSupplyCurrentLog.append(tongueMotor.getSupplyCurrent().getValueAsDouble());
+        //ClimberLeftOutputCurrentLog.append(armMotorLeft.getSupplyCurrent().getValueAsDouble());
+        //ClimberRightOutputCurrentLog.append(armMotorRight.getSupplyCurrent().getValueAsDouble());
+        //ClimberAlignOutputCurrentLog.append(alignMotor.getSupplyCurrent().getValueAsDouble());
+        //ClimberToungueOutputCurrentLog.append(tongueMotor.getSupplyCurrent().getValueAsDouble());
 
-        SmartDashboard.putBoolean("Arm Limit Switch", getArmLimit());
-        SmartDashboard.putBoolean("Tongue Limit Switch", getTongueLimit());
-        SmartDashboard.putBoolean("Alignment Limit Switch", getAlighnmentLimit());
-        SmartDashboard.putNumber("Arm 1 Encoder Position", getArm1EncoderPosition());
-        SmartDashboard.putNumber("Arm 2 Encoder Position", getArm1EncoderPosition());
-        SmartDashboard.putNumber("Alignment Encoder Position", getArm1EncoderPosition());
-        SmartDashboard.putNumber("Tongue Encoder Position", getArm1EncoderPosition());
+        //SmartDashboard.putBoolean("Arm Limit Switch", getArmLimit());
+        //SmartDashboard.putBoolean("Tongue Limit Switch", getTongueLimit());
+        //SmartDashboard.putBoolean("Alignment Limit Switch", getAlighnmentLimit());
+        //SmartDashboard.putNumber("Arm 1 Encoder Position", getArm1EncoderPosition());
+        //SmartDashboard.putNumber("Arm 2 Encoder Position", getArm1EncoderPosition());
+        //SmartDashboard.putNumber("Alignment Encoder Position", getArm1EncoderPosition());
+        //SmartDashboard.putNumber("Tongue Encoder Position", getArm1EncoderPosition());
+
     }
 }
 
