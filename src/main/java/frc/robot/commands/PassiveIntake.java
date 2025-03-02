3e39df14e4726ea77db78a679f6f24c681daf5df
Ashwin Shrivastav <103900575+ASH-will-WIN@users.noreply.github.com>
2025-03-02 15:53:16 -0500

    Finished javadocs and commands

--- src/main/java/frc/robot/commands/PassiveIntake.java
+++ src/main/java/frc/robot/commands/PassiveIntake.java
@@ -11,14 +11,23 @@ import frc.robot.subsystems.Manipulator.ManipulatorStates;
 public class PassiveIntake extends Command {
     public PassiveIntake() {}
 
+    /**
+     * Sets manipulator state to out.
+     */
     public void execute() {
         Robot.getManipulator().setManipState(ManipulatorStates.OUT);
     }
     
+    /**
+     * Returns whether the coral is ready.
+     */
     public boolean isFinished() {
         return Robot.getManipulator().isCoralReady();
     }
 
+    /**
+     * Sets manipulator state to off. 
+     */
     public void end(boolean interuppted) {
         Robot.getManipulator().setManipState(ManipulatorStates.OFF);
     }
